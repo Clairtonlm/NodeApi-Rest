21b8746bbe0a2b981420c5933d904ef064ba23eb
Clairtonlm <clairtonlm@gmail.com>
2023-08-24 23:32:09 -0300

    Acesso ao Banco

--- src/config/dbconnect.js
+++ src/config/dbconnect.js
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
-mongoose.connect('mongodb+srv://clairtonlm:235910@notrixdev.mpbefkb.mongodb.net/livraria-node');
+mongoose.connect('mongodb+srv://clairtonlm:xxxxx@notrixdev.mpbefkb.mongodb.net/livraria-node');
 
 let db = mongoose.connection;
 
